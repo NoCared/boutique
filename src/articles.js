e08375f324259b100f3e10097377127c69fbb90e
nocared <paul.richarddu30@gmail.com>
2023-11-13 12:34:06 +0100

    ADD - Ajout des articles

---
+++ src/articles.js
@@ -0,0 +1,66 @@
+const articles = [
+    {
+        id: 0,
+        name: "Lapinou",
+        url: "./assets/img/costumes/lapin.jpg",
+        price: 400,
+        promo: true,
+        qte: 10,
+        description: "Costume 100% pure laine vierge. Anti allergène. Existe en plusieurs coloris. De grands moments pour vos soirées ..."
+    },
+    {
+        id: 1,
+        name: "Canard",
+        url: "./assets/img/costumes/shopping.webp",
+        price: 350,
+        promo: false,
+        qte: 5,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 2,
+        name: "Chaton",
+        url: "./assets/img/costumes/aerobic.jpg",
+        price: 500,
+        promo: true,
+        qte: 8,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 3,
+        name: "Mario",
+        url: "./assets/img/costumes/mario.jpg",
+        price: 500,
+        promo: true,
+        qte: 15,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 4,
+        name: "Los pollos hermanos",
+        url: "./assets/img/costumes/poulet.jpg",
+        price: 500,
+        promo: true,
+        qte: 7,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 5,
+        name: "Sushi",
+        url: "./assets/img/costumes/sushi.jpg",
+        price: 500,
+        promo: true,
+        qte: 9,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 6,
+        name: "Queen Mum",
+        url: "./assets/img/costumes/queen.jpg",
+        price: 500,
+        promo: true,
+        qte: 20,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+];
+export default articles
